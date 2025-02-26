835c7b0509b5e942d1317099969d0bb0da980662
rehadhasan <rehadhasan664@gmail.com>
2025-02-27 01:53:05 +0600

    Authorization Check

--- src/services/shop/index.ts
+++ src/services/shop/index.ts
@@ -7,6 +7,7 @@ export const createShop = async (data: FormData) => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/shop`, {
       method: "POST",
+      // for checking user
       headers: {
         Authorization: (await cookies()).get("accessToken")!.value,
       },
