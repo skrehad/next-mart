5af377cdd49421d160481c89354ac574b123277e
rehadhasan <rehadhasan664@gmail.com>
2025-02-28 01:34:46 +0600

    revalidation

--- src/components/modules/shop/create-shop/category/index.tsx
+++ src/components/modules/shop/create-shop/category/index.tsx
@@ -1,18 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import CreateCategoryModal from "./CreateCategoryModal";
 import { NMTable } from "@/components/ui/core/NMTable";
 import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
 import { Trash } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+import { deleteCategory } from "@/services/Category";
 import { ICategory } from "@/types/category";
+import DeleteConfirmationModal from "@/components/ui/core/NMModal/DeleteConfirmationModal";
 
 type TCategoriesProps = {
   categories: ICategory[];
 };
 
 const ManageCategories = ({ categories }: TCategoriesProps) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
   const handleDelete = (data: ICategory) => {
     console.log(data);
+    setSelectedId(data?._id);
+    setSelectedItem(data?.name);
+    setModalOpen(true);
+  };
+
+  const handleDeleteConfirm = async () => {
+    try {
+      if (selectedId) {
+        const res = await deleteCategory(selectedId);
+        console.log(res);
+        if (res.success) {
+          toast.success(res.message);
+          setModalOpen(false);
+        } else {
+          toast.error(res.message);
+        }
+      }
+    } catch (err: any) {
+      console.error(err?.message);
+    }
   };
 
   const columns: ColumnDef<ICategory>[] = [
@@ -71,6 +100,12 @@ const ManageCategories = ({ categories }: TCategoriesProps) => {
         <CreateCategoryModal />
       </div>
       <NMTable data={categories} columns={columns} />
+      <DeleteConfirmationModal
+        name={selectedItem}
+        isOpen={isModalOpen}
+        onOpenChange={setModalOpen}
+        onConfirm={handleDeleteConfirm}
+      />
     </div>
   );
 };
